1681e61cde50ab8d6f506c4ceb19ba4f0c56fff9
Solideo Putra Zendrato <solidzendrato@gmail.com>
2024-05-21 20:28:21 +0200

    connect chat-stream to clerk

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,17 +1,57 @@
-import { UserButton } from '@clerk/nextjs';
+"use client"
+
+import { UserButton, useUser } from '@clerk/nextjs';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Channel, ChannelHeader, ChannelList, Chat, LoadingIndicator, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import useInitializeChatClient from './useInitializeChatClient';
 
 const ChatPage = () => {
-  const { userId } = auth();
+  // const { userId } = auth();
+
+  // if(!userId) redirect("/sign-in")
   
-  if(!userId) redirect("/sign-in")
+  const chatClient = useInitializeChatClient();
+  const { user } = useUser();
+
+  if (!chatClient || !user) {
+    return (
+      <div className="flex h-screen items-center justify-center bg-gray-100 dark:bg-black">
+        <LoadingIndicator size={40} />
+      </div>
+    );
+  }
+  
 
   return (
-    <div>
-      Chat Page
+    <div className="h-screen bg-gray-100 text-black dark:bg-black dark:text-white xl:px-20 xl:py-8">
       <UserButton afterSignOutUrl='/'/>
+      <Chat client={chatClient}>
+        <div className=' flex flex-row h-full'>
+          <div className=' w-full max-w-[360px]'>
+            <ChannelList
+              filters={{
+                type: "messaging",
+                members: {$in: [user.id]}
+              }}
+              sort={{last_message_at: -1}}
+              options={{state: true, presence: true, limit: 10}}
+              />
+          </div>
+          <div className=' h-full w-full'>
+            <Channel>
+              <Window>
+                <ChannelHeader/>
+                <MessageList/>
+                <MessageInput/>
+              </Window>
+              <Thread/>
+            </Channel>
+          </div>
+        </div>
+      </Chat>
     </div>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,26 @@
-// import {
-//   clerkMiddleware,
-//   createRouteMatcher
-// } from '@clerk/nextjs/server';
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
 
-// const isProtectedRoute = createRouteMatcher([
-//   '/chat(.*)',
-// ]);
+const isProtectedRoute = createRouteMatcher([
+  '/chat(.*)',
+  '/api/get-token'
+]);
 
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
